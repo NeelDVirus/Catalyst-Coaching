0433e23c9a6f7c35be574ca3c4559933ddc223f3
Jake D'Virus <neelunstp@gmail.com>
2018-09-25 22:59:35 +0530

    banner: adAppendix: building phase: 3

--- js/general.js
+++ js/general.js
@@ -2,23 +2,43 @@
 $(document).ready(function(){
 
 	/*######################################################
-	###     Making the div square (making width same   #####
-	###     as height (header .col-brand address       #####
-	###     .wrapper-icon)                             #####
+	###                                                #####
+	###               HEADER                           #####
+	###                                                #####
 	#######################################################*/
+
+	/* (brand -- address.wrapper-icon)
+	Making the address.wrapper-icon square (making width same as height) */
+
 	var $targetBrandIcon = $(".row-brand .wrapper-icon");
 	var $targetBrandIconHeight = $targetBrandIcon.outerHeight();
 	$targetBrandIcon.css("width", $targetBrandIconHeight +"");
 
 
+	/* (nav --)
+	Overlapping(vertically) the half height 
+	of Nav section over the banner section*/
 
-	/*######################################################
-	###     Overlapping(vertically) the half          ######
-	###     part of Nav section over the       
-	###     banner section                            ######  
-	#######################################################*/
 	var $navSectionHeight = $(".col-nav").outerHeight();
-	console.log($navSectionHeight);
 	$(".wrapper-banner").css("margin-top", "-"+(0.5 * $navSectionHeight) +"px");
 	$(".wrapper-banner .row-banner.hero-image").css("padding-top", (0.5 * $navSectionHeight)+"px");
+
+
+	/*######################################################
+	###                                               ######
+	###                BANNER                         ######
+	###                                               ######  
+	#######################################################*/
+
+	/* (adAppendix -- )
+	adding negative margin-top to --> 
+	(.adAppendix .ad .iconWrapper .icon) */
+    var $adAppendixIconSubWrapper = $(".adAppendix .iconSubWrapper");
+	var $adAppendixIconSubWrapperHeight = $adAppendixIconSubWrapper.outerHeight();
+	$adAppendixIconSubWrapper.css("margin-bottom", "-"+(0.5 * $adAppendixIconSubWrapperHeight)+"px");
+	//var $adAppendixHeight =67;
+	//console.log(0.5 * $adAppendixIconSubWrapperHeight);
 })
+
+
+
