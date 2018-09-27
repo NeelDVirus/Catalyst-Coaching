c6cafbcf183014f04484004b1878f7aae6f808c7
Jake D'Virus <neelunstp@gmail.com>
2018-09-27 22:49:55 +0530

    banner: adAppendix: building phase: 3

--- js/general.js
+++ js/general.js
@@ -7,7 +7,8 @@ $(document).ready(function(){
 	###                                                #####
 	#######################################################*/
 
-	/* (brand -- address.wrapper-icon)
+
+	/**********  (brand -- address.wrapper-icon)   ***********************
 	Making the address.wrapper-icon square (making width same as height) */
 
 	var $targetBrandIcon = $(".row-brand .wrapper-icon");
@@ -15,7 +16,8 @@ $(document).ready(function(){
 	$targetBrandIcon.css("width", $targetBrandIconHeight +"");
 
 
-	/* (nav --)
+
+	/*********   (nav --)   **************
 	Overlapping(vertically) the half height 
 	of Nav section over the banner section*/
 
@@ -29,15 +31,34 @@ $(document).ready(function(){
 	###                BANNER                         ######
 	###                                               ######  
 	#######################################################*/
+	var $bannerWrapper = $(".wrapper-banner");
 
 	/* (adAppendix -- )
 	adding negative margin-top to --> 
 	(.adAppendix .ad .iconWrapper .icon) */
     var $adAppendixIconSubWrapper = $(".adAppendix .iconSubWrapper");
 	var $adAppendixIconSubWrapperHeight = $adAppendixIconSubWrapper.outerHeight();
-	$adAppendixIconSubWrapper.css("margin-bottom", "-"+(0.5 * $adAppendixIconSubWrapperHeight)+"px");
-	//var $adAppendixHeight =67;
-	//console.log(0.5 * $adAppendixIconSubWrapperHeight);
+	$adAppendixIconSubWrapper.parent().css("margin-bottom", "-"+(0.6 * $adAppendixIconSubWrapperHeight)+"px");
+	$adAppendixIconSubWrapper.parent().next().css("padding-top", (0.4 * $adAppendixIconSubWrapperHeight)+"px");
+
+
+	/**********   (.row.hero-image)   ************** 
+	adding padding-bottom to hero-image to compensate the additonal overlapping of .adAppendix row which extent beyond the .banner-wrapper bottom end */
+
+	
+	var $ad = $bannerWrapper.find(".adAppendix .ad");
+	var $adTransformImpact = $ad.outerHeight();
+	//$adAppendixIconSubWrapperHeight is in the (adAppendix Sub-Section of BANNER Section)
+	var $textWrapperMarginTop = "-1.5em"; //from css
+
+	var $requiredPaddingBottomForHeroImage =  ($adTransformImpact + $adAppendixIconSubWrapperHeight) + "-" + ($textWrapperMarginTop)
+	$bannerWrapper.children(".row.hero-image");
+
+
+
+	console.log($bannerWrapper.children(".row.hero-image"));
+
+
 })
 
 
